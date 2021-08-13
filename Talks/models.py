eb5c5ebd24b70f948fda278d168b50c97bb276c2
Jamie Phillips <phillipsja97@gmail.com>
2021-08-12 21:31:38 -0500

    updated settings.py

--- Talks/models.py
+++ Talks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Talks(models.Model):
+  talk_id = models.AutoField(primary_key=True)
+  talk_title = models.CharField(max_length=1500)
+  talk_author = models.CharField(max_length=500)
+  talk_href = models.CharField(max_length=1000)
+  talk_image = models.CharField(max_length=500)
--- djangoRestApi/settings.py
+++ djangoRestApi/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^r5!#!nimzu)1d%bkem0lev=_gh)d48(8#+tn72timz#08=s!'
+SECRET_KEY = os.getenv('SECRET_KEY_STRING')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
