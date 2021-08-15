c2a03f26b43d4e3063e6dc640913cc4fd65ad8c9
Jamie Phillips <phillipsja97@gmail.com>
2021-08-14 22:31:58 -0500

    quick update on API

--- Talks/urls.py
+++ Talks/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from Talks import views
 
 urlpatterns=[
-    url(r'^alltalks$',views.TalksApi),
+    url(r'^alltalks$',views.AllTalks),
     url(r'^talk/([0-9]+)$',views.GetTalksById)
-    # url(r'^gettalk/([0-9]+)$',views.GetTalksById)
 ]
